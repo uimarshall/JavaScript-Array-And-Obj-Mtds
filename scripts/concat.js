0c85217cc901200ea7d5b0189f62b9ba9ebf0083
uimarshall <uimarshall@gmail.com>
2021-01-10 23:37:09 +0100

    Add concat method

---
+++ scripts/concat.js
@@ -0,0 +1,8 @@
+const alphabets = ["a", "b", "c"];
+const numbers = [1, 2, 3];
+const names = ["John", "Doe"];
+
+const mergedArray = alphabets.concat(numbers, names);
+
+console.log(mergedArray);
+// Expected Output: ['a','b','c',1,2,3,'John', 'Doe']
